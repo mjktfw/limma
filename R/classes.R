8ec6db21d793f0ac26de6f8246eaa921e27827f7
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-09-16 10:36:25 +0000

    limma 2.11.13. Bug fix to as.matrix.vsn.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@27180 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/classes.R
+++ R/classes.R
@@ -177,7 +177,9 @@ as.matrix.exprSet <- function(x,...) x@exprs
 #  13 July 2006
 as.matrix.PLMset <- function(x,...) x@chip.coefs
 #  19 Dec 2006, 18 May 2007
-as.matrix.ExpressionSet <- as.matrix.LumiBatch <- as.matrix.vsn <- function(x,...) env=x@assayData[["exprs"]]
+as.matrix.ExpressionSet <- as.matrix.LumiBatch <- function(x,...) env=x@assayData[["exprs"]]
+#  16 Sep 2007
+as.matrix.vsn <- function(x,...) x@hx
 
 if(getRversion() >= "2.4.0") {
 
