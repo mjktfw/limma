4199bf63f43474e5f4e0926c2f0580b23f427ca9
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-06-16 02:37:30 +0000

    16 June 2008: limma 2.15.9
    - new capabilities and arguments for write.fit().  The argument
      method allows p-values to be adjusted globally instead of
      separately, and the argument F.adjust allows the F-statistic
      P-values to be adjusted.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@32294 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/write.R
+++ R/write.R
@@ -1,16 +1,27 @@
 #  OUTPUT
 
-write.fit <- function(fit, results=NULL, file, digits=3, adjust="none", sep="\t", ...) {
+write.fit <- function(fit, results=NULL, file, digits=3, adjust="none", method="separate", F.adjust="none", sep="\t", ...) {
 #	Write an MArrayLM fit to a file
 #	Gordon Smyth
-#	14 Nov 2003.  Last modified 24 February 2007.
+#	14 Nov 2003.  Last modified 16 June 2008.
 
 	if(!is(fit, "MArrayLM")) stop("fit should be an MArrayLM object")
 	if(!is.null(results) && !is(results,"TestResults")) stop("results should be a TestResults object")
-
 	if(is.null(fit$t) || is.null(fit$p.value)) fit <- eBayes(fit)
+	method <- match.arg(method, c("separate","global"))
+
 	p.value <- as.matrix(fit$p.value)
-	for (j in 1:ncol(p.value)) p.value[,j] <- p.adjust(p.value[,j],method=adjust)
+	if(adjust=="none") {
+		p.value.adj <- NULL
+	} else {
+		p.value.adj <- p.value
+		if(method=="separate") for (j in 1:ncol(p.value)) p.value.adj[,j] <- p.adjust(p.value[,j],method=adjust)
+		if(method=="global") p.value.adj <- p.adjust(p.value,method=adjust)
+	}
+	if(F.adjust=="none" || is.null(fit$F.p.value))
+		F.p.value.adj <- NULL
+	else
+		F.p.value.adj <- p.adjust(fit$F.p.value,method=F.adjust)
 
 	rn <- function(x,digits=digits) if(is.null(x))
 		NULL
@@ -23,8 +34,10 @@ write.fit <- function(fit, results=NULL, file, digits=3, adjust="none", sep="\t"
 	tab$Coef <- rn(fit$coef,digits=digits)
 	tab$t <- rn(fit$t,digits=digits-1)
 	tab$p.value <- rn(p.value,digits=digits+2)
+	tab$p.value.adj <- rn(p.value.adj,digits=digits+3)
 	tab$F <- rn(fit$F,digits=digits-1)
 	tab$F.p.value <- rn(fit$F.p.value,digits=digits+2)
+	tab$F.p.value.adj <-tab$F.p.value.adj <- rn(F.p.value.adj,digits=digits+3)
 	tab$Res <- unclass(results)
 	tab$Genes <- fit$genes
 	tab <- data.frame(tab,check.names=FALSE)
