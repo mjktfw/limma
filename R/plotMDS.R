08a769a23abbdde9d294e622d2cbbba4f16e790e
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-03-27 06:47:14 +0000

    27 March 2009: limma 2.17.15
    - further work on plotMDS() to make arguments and help
      page clearer.
    
    27 March 2009: limma 2.17.14
    
    - New function romer() for rotation gene set enrichment analysis.
      This can be viewed as a version of GSEA for linear models.
    - New function symbols2indices() which matches a list of
      gene sets as symbols against of vector of gene symbols.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@38258 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotMDS.R
+++ R/plotMDS.R
@@ -1,17 +1,17 @@
 ##  PLOTMDS.R
 
-plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,pcs=c(1,2),k=max(pcs),...) 
+plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),ndim=max(dim.plot),...)
 #	Multi-dimensional scaling with top-distance
 #	Di Wu and Gordon Smyth
-#	19 March 2009.  Last modified 23 March 2009.
+#	19 March 2009.  Last modified 26 March 2009.
 {
 	x <- as.matrix(x)
 	if(is.null(labels)) {labels<-1:dim(x)[2]}
 
 	nprobes <- nrow(x)
 	nsamples <- ncol(x)
-	if(k < 2) stop("Need at least two dimensions")
-	if(nsamples < k) stop("Two few samples")
+	if(ndim < 2) stop("Need at least two dim.plot")
+	if(nsamples < ndim) stop("Two few samples")
 
 #	Distance measure is mean of top squared deviations for each pair of arrays
 	dd <- matrix(0,nrow=nsamples,ncol=nsamples)
@@ -20,8 +20,8 @@ plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,pcs=c(1,2),k=max
 	for (j in 1:(i-1))
 		dd[i,j]=sqrt(mean(sort.int((x[,i]-x[,j])^2,partial=topindex)[topindex:nprobes]))
 
-	a1 <- cmdscale(as.dist(dd),k=max(pcs))
-	plot(a1[,pcs[1]],a1[,pcs[2]],type="n",xlab=paste("Dimension",pcs[1]),ylab=paste("Dimension",pcs[2]),...)
-	text(a1[,pcs[1]],a1[,pcs[2]],labels=labels,col=col,cex=cex)
+	a1 <- cmdscale(as.dist(dd),k=ndim)
+	plot(a1[,dim.plot[1]],a1[,dim.plot[2]],type="n",xlab=paste("Dimension",dim.plot[1]),ylab=paste("Dimension",dim.plot[2]),...)
+	text(a1[,dim.plot[1]],a1[,dim.plot[2]],labels=labels,col=col,cex=cex)
 	return(invisible(dd))
 }
