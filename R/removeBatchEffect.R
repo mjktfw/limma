6c3e1b15cdf4f193a27ff408dcdec8bc01cbdccf
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-08-31 08:32:06 +0000

    31 Aug 2009: limma 2.19.4
    - bug fix to removeBatchEffect(), which gave incorrect
      results when design=NULL.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@41422 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/removeBatchEffect.R
+++ R/removeBatchEffect.R
@@ -3,16 +3,16 @@
 #  A refinement would be to empirical Bayes shrink
 #  the batch effects before subtracting them.
 
-removeBatchEffect <- function(x,batch,design=NULL)
+removeBatchEffect <- function(x,batch,design=matrix(1,ncol(x),1))
 #  Remove batch effects from matrix of expression data
 #  Carolyn de Graaf and Gordon Smyth
-#  1 Aug 2008. Last revised 15 Oct 2008.
+#  1 Aug 2008. Last revised 31 Aug 2009.
 {
 	x <- as.matrix(x)
 	batch <- as.factor(batch)
 	contrasts(batch) <- contr.sum(levels(batch))
 	X <- model.matrix(~batch)[,-1,drop=FALSE]
-	if(!is.null(design)) X <- qr.resid(qr(design),X)
+	X <- qr.resid(qr(design),X)
 	qrX <- qr(X)
 	t(qr.resid(qrX,t(x)))
 }
