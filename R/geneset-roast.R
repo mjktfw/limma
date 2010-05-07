c16aaee52cd38d5656a05c837d5ff6202e9ed386
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-05-07 08:45:21 +0000

    7 May 2010: limma 3.5.3
    - new argument 'set.statistic' for roast(), which specifies
      the gene set summary statistic to be used.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@46741 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/geneset-roast.R
+++ R/geneset-roast.R
@@ -1,9 +1,9 @@
 ##  ROAST.R
 
-roast <- function(iset=NULL,y,design,contrast=ncol(design),gene.weights=NULL,array.weights=NULL,block=NULL,correlation,var.prior=NULL,df.prior=NULL,nrot=999)
+roast <- function(iset=NULL,y,design,contrast=ncol(design),set.statistic="mean",gene.weights=NULL,array.weights=NULL,block=NULL,correlation,var.prior=NULL,df.prior=NULL,nrot=999)
 # rotation gene set testing for linear models
 # Gordon Smyth and Di Wu
-# 24 Apr 2008. Revised 4 May 2010.
+# 24 Apr 2008. Revised 7 May 2010.
 {
 	if(is.null(iset)) iset <- rep(TRUE,nrow(y))
 	y <- as.matrix(y)
@@ -44,6 +44,8 @@ roast <- function(iset=NULL,y,design,contrast=ncol(design),gene.weights=NULL,arr
 		design <- backsolve(R, design, transpose = TRUE)
  	}
 
+	set.statistic <- match.arg(set.statistic,c("mean","floormean","mean50","msq"))
+
 #	Reform design matrix so that contrast of interest is last column
 	qr <- qr(contrast)
 	Q <- qr.Q(qr,complete=TRUE)
@@ -83,32 +85,23 @@ roast <- function(iset=NULL,y,design,contrast=ncol(design),gene.weights=NULL,arr
 	B <- Y[1,]
 	modt <- signc*B/sd.post
 
-	statobs <- p <- rep(0,4)
-	names(statobs) <- names(p) <- c("mixed","up","down","either")
-	stati <- array(0,c(nrot,4),dimnames=list(NULL,names(p)))
+	statobs <- p <- rep(0,3)
+	names(statobs) <- names(p) <- c("mixed","up","down")
+	statrot <- array(0,c(nrot,3),dimnames=list(NULL,names(p)))
 
 #	Convert to z-scores
 	modt <- zscoreT(modt,df=d0+d)
-#	Gene weights. Calculate active proportions	
+
+#	Active proportions	
 	if(!is.null(gene.weights)) {
 		if(length(gene.weights) != nset) stop("length of gene.weights disagrees with size of set")
-		sgw <- sign(gene.weights)
-		r1 <- mean(sgw*modt > sqrt(2))
-		r2 <- mean(sgw*modt < -sqrt(2))
-		w <- sgw*sqrt(abs(gene.weights))
-		modt <- w*modt
+		s <- sign(gene.weights)
+		r1 <- mean(s*modt > sqrt(2))
+		r2 <- mean(s*modt < -sqrt(2))
 	} else {
 		r1 <- mean(modt > sqrt(2))
 		r2 <- mean(modt < -sqrt(2))
 	}
-#	Observed statistics
-	modt2 <- modt^2
-	mp <- sum(modt2[modt>0])/nset
-	mn <- sum(modt2[modt<0])/nset
-	statobs["mixed"] <- mean(modt2) 
-	statobs["up"] <- mp
-	statobs["down"] <- mn
-	statobs["either"] <- max(c(mp,mn))
 
 #	Random rotations
 	R <- matrix(rnorm(nrot*(d+1)),nrot,d+1)
@@ -121,21 +114,87 @@ roast <- function(iset=NULL,y,design,contrast=ncol(design),gene.weights=NULL,arr
 		sdr.post <- sqrt(s02)
 	modtr <- signc*Br/sdr.post
 	modtr <- zscoreT(modtr,df=d0+d)
-	if(!is.null(gene.weights)) modtr <- t(w*t(modtr))
 
-	mp <- rowMeans(pmax(modtr,0)^2)
-	mn <- rowMeans(pmax(-modtr,0)^2)
-	stati[,"mixed"] <- rowMeans(modtr^2)
-	stati[,"up"] <- mp
-	stati[,"down"] <- mn
-	stati[,"either"] <- pmax(mp,mn)
+	if(set.statistic=="msq") {
+#		Observed statistics
+		modt2 <- modt^2
+		if(!is.null(gene.weights)) modt2 <- gene.weights*modt2
+		statobs["mixed"] <- mean(modt2)
+		statobs["up"] <- sum(modt2[modt > 0])/nset
+		statobs["down"] <- sum(modt2[modt < 0])/nset
+#		Simulated statistics   
+		if(!is.null(gene.weights)) {
+			gene.weights <- s*sqrt(abs(gene.weights))
+			modtr <- t(gene.weights*t(modtr))
+		}
+		statrot[,"mixed"] <- rowMeans(modtr^2)
+		statrot[,"up"] <- rowMeans(pmax(modtr,0)^2)
+		statrot[,"down"] <- rowMeans(pmax(-modtr,0)^2)
+#		p-values
+		p <- (rowSums(t(statrot) >= statobs)+1)/(nrot+1)
+	}
 
-#	p-values
-	p <- (rowSums(t(stati) >= statobs)+1)/(nrot+1)
+	if(set.statistic=="mean50") { 
+#		Observed statistics
+		if(!is.null(gene.weights)) modt <- gene.weights*modt
+		half <- nset %/% 2L +1L
+		meanTop <- function(x,n) mean(sort(x,partial=n)[n:length(x)])
+		statobs["mixed"] <- meanTop(abs(modt),half)
+		statobs["up"] <- meanTop(modt,half)
+		statobs["down"] <- meanTop(-modt,half)
+#		Simulated statistics
+		if(!is.null(gene.weights)) modtr <- t(gene.weights*t(modtr))
+		statrot[,"mixed"] <- apply(abs(modtr),1,meanTop,n=half)
+		statrot[,"up"] <- apply(modtr,1,meanTop,n=half)
+		statrot[,"down"] <- apply(-modtr,1,meanTop,n=half)
+#		p-values
+		p <- (rowSums(t(statrot) >= statobs) + 1)/(nrot + 1)
+	}
+   
+	if(set.statistic=="floormean") { 
+#		Observed statistics
+		chimed <- qchisq(0.5,df=1)
+		amodt <- pmax(abs(modt),chimed)
+		if(!is.null(gene.weights)) {
+			amodt <- gene.weights*amodt
+			modt <- gene.weights*modt
+		}
+		statobs["mixed"] <- mean(amodt)
+		statobs["up"] <- mean(pmax(modt,0))
+		statobs["down"] <- mean(pmax(-modt,0))
+#		Simulated statistics
+		amodtr <- pmax(abs(modtr),chimed)
+		if(!is.null(gene.weights)) {
+			amodtr <- t(gene.weights*t(amodtr))
+			modtr <- t(gene.weights*t(modtr))
+		}
+		statrot[,"mixed"] <- rowMeans(amodtr)
+		statrot[,"up"] <- rowMeans(pmax(modtr,0))
+		statrot[,"down"] <- rowMeans(pmax(-modtr,0))
+#		p-values
+		p <- (rowSums(t(statrot) >= statobs) + 1)/(nrot + 1)
+	}
+   
+	if(set.statistic=="mean") { 
+#		Observed statistics
+		if(!is.null(gene.weights)) modt <- gene.weights*modt
+		m <- mean(modt)
+		statobs["mixed"] <- mean(abs(modt))
+		statobs["up"] <- m
+		statobs["down"] <- -m
+#		Simulated statistics
+		if(!is.null(gene.weights)) modtr <- t(gene.weights*t(modtr))
+		m <- rowMeans(modtr)
+		statrot[,"mixed"] <- rowMeans(abs(modtr))
+		statrot[,"up"] <- m
+		statrot[,"down"] <- -m
+#		p-values
+		p <- (rowSums(t(statrot) >= statobs) + 1)/(nrot + 1)
+	}
 
 #	Output
-	out <- data.frame(Z=sqrt(statobs),Active=c(r1+r2,r1,r2,max(r1,r2)),P.Value=p)
-	row.names(out) <- c("mixed","up","down","either")
+	out <- data.frame(ActiveProp=c(r1+r2,r1,r2),P.Value=p)
+	row.names(out) <- c("mixed","up","down")
 	out
 }
 
