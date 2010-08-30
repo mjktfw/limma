13866e9bfec241c954e3b1e60e4cea8736f808b5
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-08-30 02:25:46 +0000

    30 August 2010: limma 2.5.19
    - The backgroundCorrect() code has been simplified and a
      new function backgroundCorrect.matrix() introduced for
      matrix arguments.  The argument 'method' now defaults to
      "auto" instead of NULL, although the effect is the same.
      None of the changes to backgroundCorrect() are user-
      visible, except that background intensities are now
      used correctly when the argument RG is an EListRaw object.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@49092 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/background.R
+++ R/background.R
@@ -2,74 +2,77 @@
 
 #  BACKGROUND CORRECTION
 
-backgroundCorrect <- function(RG, method=NULL, offset=0, printer=RG$printer, normexp.method="saddle", verbose=TRUE) {
+backgroundCorrect <- function(RG, method="auto", offset=0, printer=RG$printer, normexp.method="saddle", verbose=TRUE)
+{
 #	Apply background correction to microarray data
 #	Gordon Smyth
-#	12 April 2003.  Last modified 28 October 2009.
+#	12 April 2003.  Last modified 30 August 2010.
 
-#	Methods for matrices and one-channel data
-	if(!is.list(RG) && is.vector(RG)) RG <- as.matrix(RG)
-	if(is.matrix(RG)) {
-		if(is.null(method)) method <- "normexp"
-		method <- match.arg(method, c("none","normexp"))
-		if(method!="none") {
-			for (j in 1:ncol(RG)) {
-				x <- RG[,j]
-				out <- normexp.fit(x,method=normexp.method)
-				RG[,j] <- normexp.signal(out$par,x)
-				if(verbose) cat("Corrected array",j,"\n")
-			}
-		}
-		if(offset) RG <- RG+offset
-		return(RG)
-	}
+	if(is.null(method)) method <- "auto"
+
 	if(is(RG,"EListRaw")) {
-		RG$E <- Recall(RG$E,method=method,offset=offset,normexp.method=normexp.method,verbose=verbose)
+		RG$E <- backgroundCorrect.matrix(RG$E,RG$Eb,method=method,offset=offset,normexp.method=normexp.method,verbose=verbose)
+		RG$Eb <- NULL
 		return(RG)
 	}
 
-#	From here, RG is assumed to be two-color
-	if(is.null(method)) method <- "subtract"
-	if(is.null(RG$Rb) != is.null(RG$Gb)) stop("Background values exist for one channel but not the other")
+	if(class(RG)=="list" && !is.null(RG$R)) RG <- new("RGList",RG)
 
-#	rma is included as possible value for method for backward compatibility
-#	and is likely to be removed in a future version
-	method <- match.arg(method, c("none","subtract","half","minimum","movingmin","edwards","normexp","rma"))
-	if(method=="rma") {
-		warning("Please use normexp.method to specify rma instead")
-		method <- "normexp"
-		normexp.method <- "rma"
+	if(is(RG,"RGList")) {
+		RG$R <- backgroundCorrect.matrix(RG$R,RG$Rb,method=method,offset=offset,normexp.method=normexp.method,verbose=verbose)
+		RG$Rb <- NULL
+		RG$G <- backgroundCorrect.matrix(RG$G,RG$Gb,method=method,offset=offset,normexp.method=normexp.method,verbose=verbose)
+		RG$Gb <- NULL
+		return(RG)
 	}
 
-	if(is.null(RG$Rb) && is.null(RG$Gb)) method <- "none"
+	RG <- as.matrix(RG)
+	if(!(method %in% c("none","normexp"))) stop(method,"correction requires background intensities")
+	RG <- backgroundCorrect.matrix(RG,method=method,offset=offset,normexp.method=normexp.method,verbose=verbose)
+	return(RG)
+}
+
+
+backgroundCorrect.matrix <- function(E, Eb=NULL, method="auto", offset=0, printer=NULL, normexp.method="saddle", verbose=TRUE)
+{
+#	Apply background correction to microarray data
+#	in the form of foreground and background matrices
+#	Gordon Smyth
+#	30 August 2010.  Last modified 30 August 2010.
+
+	E <- as.matrix(E)
+	if(method=="auto") method <- ifelse(is.null(Eb),"normexp","subtract")
+	method <- match.arg(method, c("none","subtract","half","minimum","movingmin","edwards","normexp"))
+
+	if(is.null(Eb)) {
+		if(method %in% c("subtract","half","minimum","movingmin","edwards")) method <- "none"
+	} else {
+		Eb <- as.matrix(Eb)
+	}
+
 	switch(method,
 	subtract={
-		RG$R <- RG$R-RG$Rb
-		RG$G <- RG$G-RG$Gb
+		E <- E-Eb
 	},
 	half={
-		RG$R <- pmax(RG$R-RG$Rb, 0.5)
-		RG$G <- pmax(RG$G-RG$Gb, 0.5)
+		E <- pmax(E-Eb, 0.5)
 	},
 	minimum={
-		RG$R <- as.matrix(RG$R - RG$Rb)
-		RG$G <- as.matrix(RG$G - RG$Gb)
-		for (slide in 1:ncol(RG$R)) {
-			i <- RG$R[,slide] < 1e-18
+		E <- E-Eb
+		for (slide in 1:ncol(E)) {
+			i <- E[,slide] < 1e-18
 			if(any(i,na.rm=TRUE)) {
-				m <- min(RG$R[!i,slide],na.rm=TRUE)
-				RG$R[i,slide] <- m/2
-			}
-			i <- RG$G[,slide] < 1e-18
-			if(any(i,na.rm=TRUE)) {
-				m <- min(RG$G[!i,slide],na.rm=TRUE)
-				RG$G[i,slide] <- m/2
+				m <- min(E[!i,slide],na.rm=TRUE)
+				E[i,slide] <- m/2
 			}
 		}
 	},
 	movingmin={
-		RG$R <- RG$R-ma3x3.spottedarray(RG$Rb,printer=printer,FUN=min,na.rm=TRUE)
-		RG$G <- RG$G-ma3x3.spottedarray(RG$Gb,printer=printer,FUN=min,na.rm=TRUE)
+		if(is.null(printer)) {
+			E <- E-ma3x3.matrix(Eb,FUN=min,na.rm=TRUE)
+		} else {
+			E <- E-ma3x3.spottedarray(Eb,printer=printer,FUN=min,na.rm=TRUE)
+		}
 	},
 	edwards={
 #		Log-linear interpolation for dull spots as in Edwards (2003).
@@ -77,31 +80,27 @@ backgroundCorrect <- function(RG, method=NULL, offset=0, printer=RG$printer, nor
 #		spots with (0 < R-Rb < delta) is f=10% of the number spots
 #		with (R-Rb <= 0) for each channel and array.
 #		Note slight change from Edwards (2003).
-		one <- matrix(1,NROW(RG$R),1)
+		one <- matrix(1,nrow(E),1)
 		delta.vec <- function(d, f=0.1) {
 			quantile(d, mean(d<1e-16,na.rm=TRUE)*(1+f), na.rm=TRUE)
 		}
-		sub <- as.matrix(RG$R-RG$Rb)
+		sub <- E-Eb
 		delta <- one %*% apply(sub, 2, delta.vec)
-		RG$R <- ifelse(sub < delta, delta*exp(1-(RG$Rb+delta)/RG$R), sub)
-		sub <- as.matrix(RG$G-RG$Gb)
-		delta <- one %*% apply(sub, 2, delta.vec)
-		RG$G <- ifelse(sub < delta, delta*exp(1-(RG$Gb+delta)/RG$G), sub)
+		E <- ifelse(sub < delta, delta*exp(1-(Eb+delta)/E), sub)
 	},
 	normexp={
-		if(verbose) cat("Green channel\n")
-		RG$G <- Recall(RG$G-RG$Gb,method=method,normexp.method=normexp.method,verbose=verbose)
-		if(verbose) cat("Red channel\n")
-		RG$R <- Recall(RG$R-RG$Rb,method=method,normexp.method=normexp.method,verbose=verbose)
+		if(!is.null(Eb)) E <- E-Eb
+		for (j in 1:ncol(E)) {
+			if(verbose) cat("Array",j)
+			x <- E[,j]
+			out <- normexp.fit(x,method=normexp.method)
+			E[,j] <- normexp.signal(out$par,x)
+			if(verbose) cat(" corrected\n")
+		}
 	}
 	)
-	RG$Rb <- NULL
-	RG$Gb <- NULL
-	if(offset) {
-		RG$R <- RG$R+offset
-		RG$G <- RG$G+offset
-	}
-	new("RGList",unclass(RG))
+	if(offset) E <- E+offset
+	E
 }
 
 ma3x3.matrix <- function(x,FUN=mean,na.rm=TRUE,...)
