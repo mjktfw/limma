ca0af794e209dd826dcdf3391edec370826adddd
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-05-21 01:49:25 +0000

    21 May 2011: limma 3.9.5
    - plotMDS is now an S3 generic function.  This allows MDS plots to be
      redrawn with new labels without needing to repeat the distance or
      scaling calculations.
    
    - new S4 class "MDS" to hold multidimensional scaling ouput from
      plotMDS.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@55763 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotMDS.R
+++ R/plotMDS.R
@@ -1,9 +1,39 @@
 ##  PLOTMDS.R
 
-plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),ndim=max(dim.plot),gene.selection="pairwise",xlab=paste("Dimension",dim.plot[1]),ylab=paste("Dimension",dim.plot[2]),...)
+#	Class to hold multidimensional scaling output
+setClass("MDS",representation("list"))
+
+setMethod("show","MDS",function(object) {
+	cat("An object of class MDS\n")
+	print(unclass(object))
+})
+
+plotMDS <- function(x,...) UseMethod("plotMDS")
+
+plotMDS.MDS <- function(x,labels=colnames(x$distance.matrix),col=NULL,cex=1,dim.plot=x$dim.plot,xlab=paste("Dimension",dim.plot[1]),ylab=paste("Dimension",dim.plot[2]),...)
+#	Method for MDS objects
+#	Create a new plot using MDS coordinates or distances previously created
+#	Gordon Smyth
+#	21 May 2011
+{
+#	Are new dimensions requested?
+	if(!all(dim.plot==x$dim.plot)) {
+		ndim <- max(dim.plot)
+		if(ndim > ncol(x$cmdscale.out)) x$cmdscale.out <- cmdscale(as.dist(x$distance.matrix),k=ndim)
+		x$x <- x$cmdscale.out[,dim.plot[1]]
+		x$y <- x$cmdscale.out[,dim.plot[2]]
+	}
+#	Redo plot
+	plot(x$x,x$y,type="n",xlab=xlab,ylab=ylab,...)
+	if(is.null(labels)) labels <- 1:length(x$x)
+	text(x$x,x$y,labels=labels,col=col,cex=cex)
+	return(invisible(x))
+}
+
+plotMDS.default <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),ndim=max(dim.plot),gene.selection="pairwise",xlab=paste("Dimension",dim.plot[1]),ylab=paste("Dimension",dim.plot[2]),...)
 #	Multi-dimensional scaling with top-distance
 #	Di Wu and Gordon Smyth
-#	19 March 2009.  Last modified 6 May 2011.
+#	19 March 2009.  Last modified 21 May 2011.
 {
 	x <- as.matrix(x)
 
@@ -11,7 +41,7 @@ plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),
 	ok <- is.finite(x)
 	if(!all(ok)) x <- x[apply(ok,1,all),]
 
-	if(is.null(labels)) {labels<-1:dim(x)[2]}
+	if(is.null(labels)) labels<-1:dim(x)[2]
 
 	nprobes <- nrow(x)
 	nsamples <- ncol(x)
@@ -20,7 +50,8 @@ plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),
 
 	gene.selection <- match.arg(gene.selection,c("pairwise","common"))
 
-	dd <- matrix(0,nrow=nsamples,ncol=nsamples)
+	cn <- colnames(x)
+	dd <- matrix(0,nrow=nsamples,ncol=nsamples,dimnames=list(cn,cn))
 	topindex <- nprobes-top+1
 	if(gene.selection=="pairwise") {
 #		Distance measure is mean of top squared deviations for each pair of arrays
@@ -37,10 +68,8 @@ plotMDS <- function(x,top=500,labels=colnames(x),col=NULL,cex=1,dim.plot=c(1,2),
 	}
 
 	a1 <- cmdscale(as.dist(dd),k=ndim)
-	xcord <- a1[,dim.plot[1]]
-	ycord <- a1[,dim.plot[2]]
-	plot(xcord,ycord,type="n",xlab=xlab,ylab=ylab,...)
-	text(xcord,ycord,labels=labels,col=col,cex=cex)
-	return(invisible(list(distance.matrix=dd,x=xcord,y=ycord)))
+	mds <- new("MDS",list(dim.plot=dim.plot,distance.matrix=dd,cmdscale.out=a1,top=top,gene.selection=gene.selection))
+	mds$x <- a1[,dim.plot[1]]
+	mds$y <- a1[,dim.plot[2]]
+	plotMDS(mds,labels=labels,col=col,cex=cex,xlab=xlab,ylab=ylab,...)
 }
-
