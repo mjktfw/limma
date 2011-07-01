dcc7396dec3ee5a0058bf26204246355cfed04ac
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-07-01 02:14:40 +0000

    1 July 2011: limma 3.9.8
    - new argument batch2 for removeBatchEffect().
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@56477 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/removeBatchEffect.R
+++ R/removeBatchEffect.R
@@ -3,17 +3,22 @@
 #  A refinement would be to empirical Bayes shrink
 #  the batch effects before subtracting them.
 
-removeBatchEffect <- function(x,batch,design=matrix(1,ncol(x),1))
+removeBatchEffect <- function(x,batch,batch2=NULL,design=matrix(1,ncol(x),1))
 #  Remove batch effects from matrix of expression data
 #  Carolyn de Graaf and Gordon Smyth
-#  1 Aug 2008. Last revised 31 Aug 2009.
+#  Created 1 Aug 2008. Last revised 31 Aug 2009.
 {
 	x <- as.matrix(x)
 	batch <- as.factor(batch)
 	contrasts(batch) <- contr.sum(levels(batch))
-	X <- model.matrix(~batch)[,-1,drop=FALSE]
+	if(is.null(batch2)) {
+		X <- model.matrix(~batch)[,-1,drop=FALSE]
+	} else {
+		batch2 <- as.factor(batch2)
+		contrasts(batch2) <- contr.sum(levels(batch2))
+		X <- model.matrix(~batch+batch2)[,-1,drop=FALSE]
+	}
 	X <- qr.resid(qr(design),X)
-	qrX <- qr(X)
-	t(qr.resid(qrX,t(x)))
+	t(qr.resid(qr(X),t(x)))
 }
 
