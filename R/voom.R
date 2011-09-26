6f9637d60d72603514a2b34518327c8d19d841e4
smyth@wehi.edu.au <smyth@wehi.edu.au@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-26 01:03:16 +0000

    26 Sep 2011: limma 3.9.20
    - voom() now gives special treatment to DGEList objects.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@58372 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/voom.R
+++ R/voom.R
@@ -2,23 +2,33 @@ voom <- function(counts,design=NULL,lib.size=NULL,normalize.method="none",plot=F
 # Linear modelling of count data mean-variance modelling at the observational level.
 # Creates an EList object for entry to lmFit() etc in the limma pipeline.
 # Gordon Smyth and Charity Law
-# Created 22 June 2011.  Last modified 25 Sep 2011.
+# Created 22 June 2011.  Last modified 26 Sep 2011.
 {
-#	Fit linear model to log2-counts-per-million
-	counts <- as.matrix(counts)
+	out <- list()
+
+#	Check inputs
+	if(is(counts,"DGEList")) {
+		out$genes <- counts$genes
+		if(is.null(design) && diff(range(as.numeric(counts$sample$group)))>0) design <- model.matrix(~group,data=counts$samples)
+		if(is.null(lib.size)) lib.size <- with(counts$samples,lib.size*norm.factors)
+		counts <- counts$counts
+	} else {
+		counts <- as.matrix(counts)
+	}
 	if(is.null(design)) {
 		design <- matrix(1,ncol(counts),1)
 		rownames(design) <- colnames(counts)
 		colnames(design) <- "GrandMean"
 	}
 	if(is.null(lib.size)) lib.size <- colSums(counts)
+
+#	Fit linear model to log2-counts-per-million
 	y <- t(log2(t(counts+0.5)/(lib.size+1)*1e6))
 	y <- normalizeBetweenArrays(y,method=normalize.method)
 	fit <- lmFit(y,design,...)
 	if(is.null(fit$Amean)) fit$Amean <- rowMeans(y,na.rm=TRUE)
 
 #	Fit lowess trend to sqrt-standard-deviations by log-count-size
-#	sx <- 2^(0.25*(fit$Amean+mean(log2(lib.size+0.5))-log2(1e6)))
 	sx <- fit$Amean+mean(log2(lib.size+1))-log2(1e6)
 	sy <- sqrt(fit$sigma)
 	allzero <- rowSums(counts)==0
@@ -45,13 +55,18 @@ voom <- function(counts,design=NULL,lib.size=NULL,normalize.method="none",plot=F
 
 #	Find individual quarterroot fitted counts
 	fitted.cpm <- 2^(fit$coef %*% t(fit$design))
-	fitted.c <- 1e-6 * t(t(fitted.cpm)*(lib.size+1))
-#	fitted.qrc <- fitted.c^0.25
-	fitted.qrc <- log2(fitted.c)
+	fitted.count <- 1e-6 * t(t(fitted.cpm)*(lib.size+1))
+	fitted.logcount <- log2(fitted.count)
 
 #	Apply trend to individual observations
-	w <- 1/f(fitted.qrc)^4
-	dim(w) <- dim(fitted.qrc)
-	new("EList",list(E=y,weights=w,design=design,lib.size=lib.size))
+	w <- 1/f(fitted.logcount)^4
+	dim(w) <- dim(fitted.logcount)
+
+#	Output
+	out$E <- y
+	out$weights <- w
+	out$design <- design
+	out$lib.size <- lib.size
+	new("EList",out)
 }
 
